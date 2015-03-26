ba712a423d990e051a74dee61d5d42b9a3e1c03b
simoneg90 <gelli.simone@alice.it>
2015-03-26 10:49:31 +0100

    adding cuts.h

--- analysis_cut.C
+++ analysis_cut.C
@@ -4,12 +4,17 @@
 #include <sstream>
 
 #include "utility.cc"
+#include "cuts.h"
 
 void analysis_cut(){
 
   bool isData = false;
   const char* path="/cmshome/gellisim/MonoJet/MonoJet7XTrees/dmVM10/tree.root"; //path to analyse....maybe better to put it from outside
 
+
+  std::cout<<jet1ETA<<std::endl;
+
+  std::string test="21";
   std::stringstream str; //string for cuts
 
   breakLine();
@@ -18,8 +23,9 @@ void analysis_cut(){
 
   str<< (!isData ? "puwgt * wgt *" : "")<<"(";  //remember to rescale also for Luminosity and K-factor (1.23 for W-events, 1.27 for Z-events)
 
+  std::string jet1_cut= str.str() + "njets=1 && signaljetpt>" + jet1PT + " && abs(signajeteta)<" + jet1ETA + " && signaljetCHfrac>0.2 && signaljetNHfrac<0.7 && signaljetEMfrac<0.7)";
 
-
+  std::cout<<"Prova stringa: "<<jet1_cut<<std::endl;
 
 
 
---
+++ cuts.h
@@ -0,0 +1,8 @@
+//This header file contains only the values for cuts applied in analyis_cut.C 
+//REMEMBER the " "!
+
+#define MET "200"
+#define jet1PT "110"
+#define jet1ETA "2.5"
+#define jet2PT "30"
+#define jet2ETA "4.5"
